316e794f7b5e3df4e95175a45a5fb8b12f85cb4f
Bryan C. Mills <bcmills@google.com>
2016-07-15 14:54:39 -0400

    sync: delete dummy builders_test file
    The builders should be happy building errgroup instead now.
    
    Change-Id: I99f549032f771e067b5f7780eba1cc6e91bbdfa5
    Reviewed-on: https://go-review.googlesource.com/24962
    Run-TryBot: Bryan Mills <bcmills@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- builders_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2016 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Temporary file to make the builders happy so there's
-// something buildable in this repo. This can be deleted
-// when other code is added here.
-
-package builders_test
