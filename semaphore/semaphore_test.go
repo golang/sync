ee312a9aeaf41da5261fc9d103bffe1009ea8745
徐志强 <652732310@qq.com>
2020-03-15 08:01:34 +0800

    add TestAllocCancelDoesntStarve

--- semaphore/semaphore_test.go
+++ semaphore/semaphore_test.go
@@ -169,3 +169,44 @@ func TestLargeAcquireDoesntStarve(t *testing.T) {
 	sem.Release(n)
 	wg.Wait()
 }
+
+func TestAllocCancelDoesntStarve(t *testing.T) {
+	sem := semaphore.NewWeighted(10)
+
+	// hold 1 read lock
+	sem.Acquire(context.Background(), 1)
+
+	go func() {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*300)
+		defer cancelFunc()
+		// start a write lock request that will giveup after 300ms
+		err := sem.Acquire(ctx, 10)
+		if err == nil {
+			t.FailNow()
+		}
+	}()
+
+	// sleep 100ms, long enough for the Lock request to be queued
+	time.Sleep(time.Millisecond * 100)
+
+	// this channel will be closed if the following RLock succeeded
+	doneCh := make(chan struct{})
+	go func() {
+		// try to grab a read lock, it will be queued after the Lock request
+		// but should be notified when the Lock request is canceled
+		// this doesn't happen because there's a bug in semaphore
+		err := sem.Acquire(context.Background(), 1)
+		if err != nil {
+			t.FailNow()
+		}
+		sem.Release(1)
+		close(doneCh)
+	}()
+
+	// because of the bug in semaphore, doneCh is never closed
+	select {
+	case <-doneCh:
+	case <-time.After(time.Second):
+		t.FailNow()
+	}
+}
